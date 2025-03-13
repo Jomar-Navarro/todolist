91496ab1ebc3f7161b223e76b703a2f479b1acaf
Jomar-Navarro <Jomarnavarro201@gmail.com>
2025-03-13 13:56:43 +0100

    simplify body class in layout.js by removing unused styles

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className="bg-gray-100 text-gray-900 min-h-screen flex flex-col">
+			<body className="min-h-screen flex flex-col">
 				<main className="flex-grow container mx-auto p-4">{children}</main>
 				<Footer />
 			</body>
