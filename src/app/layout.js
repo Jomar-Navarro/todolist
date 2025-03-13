782132753e87d53ec454ea78ed57d8a293c8a246
Jomar-Navarro <Jomarnavarro201@gmail.com>
2025-03-13 13:55:45 +0100

    remove Navbar component; update Footer styles in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 import "./globals.css";
-import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export const metadata = {
@@ -11,7 +10,6 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className="bg-gray-100 text-gray-900 min-h-screen flex flex-col">
-				<Navbar />
 				<main className="flex-grow container mx-auto p-4">{children}</main>
 				<Footer />
 			</body>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer className="text-center p-4 bg-gray-950 text-white-700 mt-10">
+		<footer className="text-center p-4 text-white-700 mt-10">
 			© {new Date().getFullYear()} To-Do App | Creato con ❤️ in Next.js
 		</footer>
 	);
