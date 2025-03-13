87a451e5a9173797d6610f64ccaf45a7dd9ae12c
Jomar-Navarro <Jomarnavarro201@gmail.com>
2025-03-13 18:50:51 +0100

    capitalize month in TodoList date display; adjust input padding

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,17 @@
 export default function TodoList() {
 	const date = new Date();
 	const month = date.toLocaleString("default", { month: "long" });
+	const capitalizedMonth = month.charAt(0).toUpperCase() + month.slice(1);
 	const day = date.getDate();
 	const year = date.getFullYear();
 	return (
 		<>
 			<div className="w-full">
 				<h1 className="text-3xl font-bold">
-					Today {day} {month} {year}
+					Today {day} {capitalizedMonth} {year}
 				</h1>
 				<input
-					className="w-full my-5 p-3 border border-gray-300 rounded-3xl"
+					className="w-full my-5 p-2.5 border border-gray-300 rounded-3xl"
 					type="text"
 					placeholder="Add a new task"
 				/>
