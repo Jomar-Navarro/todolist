a3a6d0ebfe8844d60d77c47c9045523e88587124
Jomar-Navarro <Jomarnavarro201@gmail.com>
2025-03-13 18:45:49 +0100

    update TodoList component to display current date and add input for new tasks; adjust global styles for dark mode

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,20 @@
 export default function TodoList() {
+	const date = new Date();
+	const month = date.toLocaleString("default", { month: "long" });
+	const day = date.getDate();
+	const year = date.getFullYear();
 	return (
 		<>
-			<h1>Todo List</h1>
+			<div className="w-full">
+				<h1 className="text-3xl font-bold">
+					Today {day} {month} {year}
+				</h1>
+				<input
+					className="w-full my-5 p-3 border border-gray-300 rounded-3xl"
+					type="text"
+					placeholder="Add a new task"
+				/>
+			</div>
 		</>
 	);
 }
